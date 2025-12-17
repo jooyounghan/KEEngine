292dac07dacdf03864c0f70b4019e9f6ba4175d4
YounghanJoo <rsfnqf98@naver.com>
2025-12-18 00:39:12 +0900

    Create Reflect Serializer Interface

--- KEEngine/KEEngine/ReflectSystem/ReflectSerializer.h
+++ KEEngine/KEEngine/ReflectSystem/ReflectSerializer.h
@@ -1,2 +1,16 @@
 #pragma once
-#include "Xml"
+#include "File.h"
+#include "XMLReader.h"
+#include "ReflectObject.h"
+
+namespace ke
+{
+	template<typename ObjectType>
+	class ReflectSerializer
+	{
+	public:
+		void SerializeToXML(IBuffer* outBuffer, const ReflectObject<ObjectType>& obj);
+		void DeserializeFromXML(const IBuffer* const outBuffer, ReflectObject<ObjectType>& obj);
+
+	};
+}
